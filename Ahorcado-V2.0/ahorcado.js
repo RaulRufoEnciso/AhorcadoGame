f3a9e37b1750dd2fc6c72e0d97561a3937bd927a
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2023-11-11 19:36:23 +0100

    inicio del proyecto con UI

---
+++ Ahorcado-V2.0/ahorcado.js
@@ -0,0 +1,13 @@
+function ahorcado(){
+    let palabra = prompt("Escribe la palabra a buscar:");
+    console.log(`La palabra a buscar es: ${palabra}`);
+    
+}
+
+function exit(){
+    
+}
+
+function estadisticas(){
+
+}
